32bfd125a014ded685246ba73948302723fc567f
Marcos Campos <marxmyers@icloud.com>
2022-01-26 01:24:57 -0300

    Requisito 1 concluído

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const arraySpecie = ids.map((element) => species.find((specie) => specie.id === element));
+  return arraySpecie;
 }
+console.log(getSpeciesByIds());
+// Requisito concluído com exemplos tratados na mentoria.
 
 module.exports = getSpeciesByIds;
