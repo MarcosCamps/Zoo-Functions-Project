8ea58c6f63509f38088d79da8ca467b5545811a7
Marcos Campos <marxmyers@icloud.com>
2022-01-28 22:53:47 -0300

    Requisito do 1 ao 10 (menos 7 e 8) com lint e comentários

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+
 }
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+// pegar o nome da especie usando o find.
+function speciesName(id) {
+  return species.find((specie) => specie.id === id).name;
+}
+// pegar a localização usando o find.
+function speciesLocation(id) {
+  return species.find((specie) => specie.id === id).location;
 }
 
+function noParameter() {
+  const arr = [];
+  employees.forEach((element) => {
+    arr.push({
+      id: element.id,
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: element.responsibleFor.map((responsible) => speciesName(responsible)),
+      locations: element.responsibleFor.map((responsible) => speciesLocation(responsible)),
+    });
+  });
+  return arr;
+}
+
+function getEmployeesCoverage(obj) {
+  if (typeof obj === 'undefined') return noParameter();
+  const employee = employees
+    .find((employe) => employe.firstName === obj.name || employe
+      .lastName === obj.name
+      || employe.id === obj.id);
+  if (typeof employee === 'undefined') throw new Error('Informações inválidas');
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((responsible) => speciesName(responsible)),
+    locations: employee.responsibleFor.map((responsible) => speciesLocation(responsible)),
+  };
+}
+// Projeto finalizado com pesquisas e auxilio de mentoria.
 module.exports = getEmployeesCoverage;
